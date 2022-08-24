38a732349cabe1eda679c02152e019f696ff38d9
L0NG1NG <610306739@qq.com>
2022-08-24 08:30:33 +0800

    表达式计算,初见成效 加入字体

--- app/src/main/java/com/longing/mycalculator/MainActivity.kt
+++ app/src/main/java/com/longing/mycalculator/MainActivity.kt
@@ -7,18 +7,30 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusOrder
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
+import com.longing.mycalculator.exprk.Expressions
 import com.longing.mycalculator.ui.theme.*
+import java.math.RoundingMode
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,11 +50,8 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
 }
 
-
-data class CalculateState(val num1: Int = 0, val num2: Int = 0, val opt: String? = null)
 
 val buttons = arrayOf(
     arrayOf("C" to LightGray, "+/-" to LightGray, "%" to LightGray, "/" to Orange),
@@ -52,36 +61,68 @@ val buttons = arrayOf(
     arrayOf("0" to DarkGray, "." to DarkGray, "=" to Orange),
 
     )
+val robotFontFamily = FontFamily(
+    Font(R.font.roboto_light)
+)
 
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun Calculator() {
     //remember保证下次重组时数据不进行改变
-    var state by remember {
-        mutableStateOf(CalculateState())
+    var inputText by remember {
+        mutableStateOf(TextFieldValue(text = ""))
     }
+
+    var result by remember {
+        mutableStateOf("")
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Column(
         Modifier
             .background(Background)
             .padding(10.dp)
     ) {
-        Box(
+        Column(
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.Bottom,
             modifier = Modifier
+                .fillMaxWidth()
                 .fillMaxHeight(0.3f)
-                .fillMaxWidth(),
-            contentAlignment = Alignment.BottomEnd,
         ) {
 
-            Text(
-                text = state.num2.toString(), fontSize = 100.sp,
-                color = Color.White
+            TextField(
+                value = inputText,
+                textStyle = TextStyle(
+                    fontSize = 46.sp,
+                    fontFamily = robotFontFamily,
+                    textAlign = TextAlign.End
+                ),
+                onValueChange = { inputText = it },
+                modifier = Modifier.onFocusEvent { keyboardController?.hide() },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
             )
+
+            Text(
+                text = result, fontSize = 24.sp,
+                fontFamily = robotFontFamily,
+                color = Color.White,
+                textAlign = TextAlign.End
+            )
+
         }
+
         Spacer(Modifier.height(48.dp))
         Column(Modifier.fillMaxSize()) {
             buttons.forEach {
                 Row(
-                    Modifier
-                        .weight(1f),
+                    Modifier.weight(1f),
                     horizontalArrangement = Arrangement.spacedBy(10.dp)
                 ) {
                     it.forEach { symbol ->
@@ -92,33 +133,31 @@ fun Calculator() {
                                 .aspectRatio(if (symbol.first == "0") 2f else 1f),
                             symbol.first
                         ) {
-                            state = calculate(state, symbol.first)
+                            when (symbol.first) {
+                                "=" -> {
+                                    result = ""
+                                    inputText = TextFieldValue(performCalculate(inputText.text))
+
+                                }
+                                "C" -> {
+                                    result = ""
+                                    inputText = TextFieldValue("")
+
+                                }
+                                else -> {
+                                    result = performCalculate(inputText.text)
+                                    inputText = TextFieldValue(text = inputText.text + symbol.first)
+
+                                }
+                            }
                         }
                     }
-
                 }
             }
         }
     }
 }
 
-fun calculate(curState: CalculateState, input: String): CalculateState {
-    return when (input) {
-        in "0".."9" -> curState.copy(num2 = input.toInt(), num1 = curState.num2)
-        in arrayOf("+", "-", "*", "/", "%") -> curState.copy(opt = input)
-        "=" -> when (curState.opt) {
-            "+" -> curState.copy(num2 = curState.num1 + curState.num2)
-            "-" -> curState.copy(num2 = curState.num1 - curState.num2)
-            "*" -> curState.copy(num2 = curState.num1 * curState.num2)
-            "/" -> curState.copy(num2 = curState.num1 / curState.num2)
-            "%" -> curState.copy(num2 = curState.num1 % curState.num2)
-            else -> curState
-        }
-
-        "C" -> curState.copy(num1 = 0, num2 = 0, opt = null)
-        else -> curState
-    }
-}
 
 @Composable
 fun CalculatorButton(modifier: Modifier, symbol: String, onClick: () -> Unit = {}) {
@@ -130,5 +169,32 @@ fun CalculatorButton(modifier: Modifier, symbol: String, onClick: () -> Unit = {
         contentAlignment = Alignment.Center
     ) {
         Text(text = symbol, fontSize = 40.sp, color = Color.White)
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyCalculatorTheme {
+        Calculator()
+    }
+}
+
+
+fun performCalculate(expressions: String): String {
+    val operators = arrayOf('+', '-', '*', '/')
+    if (!operators.any { expressions.contains(it) }) {
+        return ""
+    }
+    if (operators.any { expressions.endsWith(it) }) {
+        return ""
+    }
+    val eval = Expressions().eval(expressions)
+
+    return if (eval.toString().contains(".")) {
+        val rounded = eval.setScale(10, RoundingMode.UP).stripTrailingZeros()
+        rounded.toString()
+    } else {
+        eval.toString()
     }
 }
