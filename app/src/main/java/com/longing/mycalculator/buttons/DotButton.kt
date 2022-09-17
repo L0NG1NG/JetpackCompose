2fb4e92d824de435af5574acf80448e8073045dc
L0NG1NG <610306739@qq.com>
2022-09-17 10:04:19 +0800

    完善小数点运算

--- app/src/main/java/com/longing/mycalculator/buttons/DotButton.kt
+++ app/src/main/java/com/longing/mycalculator/buttons/DotButton.kt
@@ -15,73 +15,74 @@ private const val TAG = "DotButton"
 
 class DotButton : Button(label = ".", fontSize = 28.sp) {
     override fun onClick(data: CalculateData) {
+
         val inputText = data.inputText
         if (inputText.text.isBlank()) {
-            buildAnnotatedString {
+            val defaultText = buildAnnotatedString {
                 withStyle(SpanStyle(color)) {
-                    append("0$label")
+                    append("0${label}")
                 }
             }
-        } else {
-            //判断数字有没有小数点
-            val expression = Computer.divideExpression(inputText)
-            val left = expression.first
-            val right = expression.second
+            data.inputText = TextFieldValue(
+                defaultText,
+                TextRange(defaultText.length)
+            )
+            return
+        }
 
-            val number: String
-            var startIndex = -1
-            var endIndex = -1
+        var startIndex = -1
+        var endIndex = -1
 
-            OperatorType.values().forEach {
-                if (startIndex < 0) {
-                    val index = left.text.lastIndexOf(it.label)
-                    if (index > startIndex) startIndex = index
-                }
-                if (endIndex < 0) {
-                    val index = right.text.lastIndexOf(it.label)
-                    if (index > endIndex) endIndex = index
-                }
+        //判断数字有没有小数点
+        val expression = Computer.divideExpression(inputText)
+        val left = expression.first
+        val right = expression.second
+        val cursorIndex = left.length
+
+        OperatorType.values().forEach {
+            if (startIndex < 0) {
+                val index = left.text.lastIndexOf(it.label)
+                if (index > startIndex) startIndex = index
             }
-            Log.d(TAG, "startIndex: $startIndex")
-            Log.d(TAG, "endIndex: $endIndex")
-
-            if (startIndex == left.length-1) {
-                //"光标前面刚好是运算符补0
-                number = left.text + "0$label" + right.text
-
-            } else if (startIndex < 0) {
-                number = if (endIndex < 0) {
-                    //纯纯的数字
-                    inputText.text
-
-                } else {
-                    left.text + right.text.subSequence(0, endIndex)
-                }
-            } else {
-                number = if (endIndex > 0) {
-                    left.subSequence(startIndex, left.length).text + right.subSequence(
-                        0,
-                        endIndex
-                    ).text
-
-                } else {
-                    inputText.text.subSequence(startIndex, left.length + endIndex).toString()
-                }
-            }
-
-            if (number.contains(label)) {
-                val newExpression = buildAnnotatedString {
-                    append(left)
-                    withStyle(SpanStyle(color)) {
-                        append(label)
-                    }
-                    append(right)
-                }
-                data.inputText =
-                    TextFieldValue(newExpression, TextRange(left.length + 1))
-                data.outputText =
-                    Computer.performCalculate(newExpression.text)
+            if (endIndex < 0) {
+                val index = right.text.lastIndexOf(it.label)
+                if (index > endIndex) endIndex = index
             }
         }
+
+        if (endIndex < 0) {
+            startIndex += 1
+            endIndex = inputText.text.length
+        } else {
+            startIndex += 1
+            endIndex += left.length
+        }
+        val number: String = inputText.text.substring(startIndex, endIndex)
+        Log.d(TAG, "startIndex: $startIndex")
+        Log.d(TAG, "endIndex: $endIndex")
+        Log.d(TAG, "当前光标所在数字：${number}")
+        //已经含有小数点不做处理
+        if (number.contains(label)) return
+        val newExpression = buildAnnotatedString {
+            if (cursorIndex == inputText.text.indexOf(number)) {
+                //光标刚好位于数字开头 补0
+                append(left.plus(buildAnnotatedString {
+                    withStyle(SpanStyle(color)) {
+                        append("0")
+                    }
+                }))
+            } else {
+                append(left)
+            }
+            withStyle(SpanStyle(color)) {
+                append(label)
+            }
+            append(right)
+        }
+        data.inputText =
+            TextFieldValue(newExpression, TextRange(left.length + 1))
+        data.outputText =
+            Computer.performCalculate(newExpression.text)
+
     }
 }
